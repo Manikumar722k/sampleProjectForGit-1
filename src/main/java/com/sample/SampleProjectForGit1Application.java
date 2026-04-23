b935bf4a16aec14e1d9ef8afeafc741843544bc1
Manikumar722k <pmanikumar722k@gmail.com>
2026-04-23 17:37:12 +0530

    initial commit

---
+++ src/main/java/com/sample/SampleProjectForGit1Application.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectForGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectForGit1Application.class, args);
+	}
+
+}
